293f0b7dd3807d20bf420defc0522c30d3ec11a0
Savkin Maxim <sawkin.max2016@yandex.ru>
2024-02-05 22:04:26 +0300

    Created api service

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const booksData = require('./data/books.json');
+
+const app = express();
+
+app.use(cors());
+
+function getRandomBook() {
+    const randomIndex = Math.floor(Math.random() * booksData.length);
+    const randomBook = booksData[randomIndex];
+    return randomBook;
+}
+
+app.get('/random-book', (req, res) => {
+    res.json(getRandomBook());
+});
+
+app.get('/random-book-delayed', (req, res) => {
+    setTimeout(() => {
+        res.json(getRandomBook());
+    }, 2000);
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
