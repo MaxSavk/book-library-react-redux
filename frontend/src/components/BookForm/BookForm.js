fd621b5a7b8cfdc75a0e5808a92dfd4e7a15701b
Savkin Maxim <sawkin.max2016@yandex.ru>
2024-02-06 05:55:47 +0300

    Add book source

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -15,14 +15,14 @@ const BookForm = () => {
         const randomIndex = Math.floor(Math.random() * booksData.length);
         const randomBook = booksData[randomIndex];
 
-        dispatch(addBook(createBookWithId(randomBook)));
+        dispatch(addBook(createBookWithId(randomBook, "random")));
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if (title && author) {
-            dispatch(addBook(createBookWithId({ title, author })));
+            dispatch(addBook(createBookWithId({ title, author }, "manual")));
             setTitle("");
             setAuthor("");
         }
@@ -32,7 +32,7 @@ const BookForm = () => {
         try {
             const res = await axios.get("http://localhost:4000/random-book");
             if (res.data && res.data.title && res.data.author) {
-                dispatch(addBook(createBookWithId(res.data)));
+                dispatch(addBook(createBookWithId(res.data, "API")));
             }
         } catch (error) {
             console.log("Error fetching random book", error);
--- frontend/src/utils/createBookWithId.js
+++ frontend/src/utils/createBookWithId.js
@@ -1,11 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
 
-const createBookWithId = (book) => {
+const createBookWithId = (book, source) => {
     return {
         ...book,
+        source,
         isFavorite: false,
         id: uuidv4(),
     };
 };
 
-export default createBookWithId
+export default createBookWithId;
